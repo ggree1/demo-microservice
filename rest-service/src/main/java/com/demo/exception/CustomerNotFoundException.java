fe1ec10aa3c22097f56c6b22401f2f820d6f7752
ggree1 <ggree1@naver.com>
2019-03-11 22:58:47 +0900

    added rest controller and test

---
+++ rest-service/src/main/java/com/demo/exception/CustomerNotFoundException.java
@@ -0,0 +1,15 @@
+package com.demo.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    private final Long id;
+
+    public CustomerNotFoundException(Long id) {
+        super("customer-not-found-" + id);
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return id;
+    }
+}
